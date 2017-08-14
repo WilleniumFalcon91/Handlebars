2153fba1b65768348a7e7a216e574d1a2c2b93cb
William Elliott <wcelliott1991@gmail.com>
2017-08-14 16:16:05 -0400

    allows modifications to the database

--- routes/member.js
+++ routes/member.js
@@ -15,4 +15,47 @@ router.get('/', function(req, res, next) {
    });
 });
 
+router.get('/:id', function(req, res, next) {
+    db.one(`
+        select * from cd.members where memid=${req.params.id};
+    `).then((result) => {
+        console.log(result);
+        res.render('member', { 
+            members: [result]
+        });
+   }).catch(console.log);
+});
+
+router.get('/:id/edit', function(req, res, next) {
+    db.one(`
+        select * from cd.members where memid=${req.params.id};
+    `).then((result) => {
+        console.log(result);
+        res.render('members-form', { 
+            member: result
+        });
+   }).catch(console.log);
+});
+
+router.post('/:id/edit', function(req, res, next) {
+  console.log(req.body);
+
+  // TODO: update all the keys
+  db.result(`
+    update cd.members
+      set
+      surname='${req.body.surname}'
+      where memid=${req.params.id};
+  `).then((result) => {
+    // db.one(`
+      // select * from cd.members where memid=${req.params.id};
+    // `).then( (result) => {
+      console.log(result);
+      res.render('members-form', {
+        member: result
+      });
+    // });
+  }).catch(console.log)
+});
+
 module.exports = router;

98d59ff6de5114b4f8307ec330f86524bac1efe5
William Elliott <wcelliott1991@gmail.com>
2017-08-14 13:59:57 -0400

    pulls all members from database

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var facilities = require('./routes/facilities');
+var members = require('./routes/member');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/facilities', facilities);
+app.use('/member', members);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/member.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const db = require('../db');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    db.query(`
+        select * from cd.members;
+    `).then((result) => {
+        console.log(result);
+        res.render('member', { 
+            members: result
+        });
+   });
+});
+
+module.exports = router;

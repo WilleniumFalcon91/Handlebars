312b80e75c2c0fadb8d744a201e67b8150eb4f29
William Elliott <wcelliott1991@gmail.com>
2017-08-14 13:38:30 -0400

    successfully templates info from database

--- routes/facilities.js
+++ routes/facilities.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../db');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('facilities', { 
-    name: 'Tennis Court',
-    booking: 'Booked by Bob at 2:30pm'
+router.get('/:id', function(req, res, next) {
+    db.one(`
+        select * from cd.facilities where facid=${req.params.id};
+    `).then((result) => {
+        console.log(result);
+        res.render('facilities', { 
+            facility: result
+        });
    });
 });
 
